986dc4be0435cdd31d826c59da0a4a08651da085
Parwiz Rahimi <farihajan@Parwizs-MacBook-Pro.local>
2017-11-11 13:52:24 -0500

    files added

---
+++ app/data/friends.js
@@ -0,0 +1,105 @@
+
+
+var friends = [
+  {
+    name: "ShahRukhan",
+    photo: "http://www.bollywoodlife.com/wp-content/uploads/2016/03/681135.jpg",
+        
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Ashwaraya-rai",
+    photo: "http://www.aishwarya-spice.com/gallery/albums/images/photoshoots/17loreapparis3/17lorealparisaps0002.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Pryanka chopra",
+    photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxNzUwNjExOV5BMl5BanBnXkFtZTcwNDUyMTUxNw@@._V1_UY1200_CR488,0,630,1200_AL_.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Kareena_kapoor",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/b/b7/Kareena_Kapoor_in_2015.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Alia_bat",
+    photo: "https://pbs.twimg.com/profile_images/536808607501647872/z7rFHRoE.jpeg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Deepita",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Deepika_Padukone_at_Piku_event.jpg/170px-Deepika_Padukone_at_Piku_event.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+
+module.exports = friends;
